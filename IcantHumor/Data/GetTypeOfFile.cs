749ca32da1db57dfffd063a506c164f6fd043e6a
Paikerite <norkov900@gmail.com>
2023-08-05 13:58:20 +0300

    some fixes

---
+++ IcantHumor/Data/GetTypeOfFile.cs
@@ -0,0 +1,28 @@
+﻿using IcantHumor.Models.Enums;
+
+namespace IcantHumor.Data
+{
+    public static class GetTypeFile
+    {
+        public static TypeOfFile GetTypeOfFile(string fileName)
+        {
+            //Accept=".png, .jpg, .jpeg, .gif, .mp4"
+            string Extension = Path.GetExtension(fileName);
+            switch (Extension)
+            {
+                case ".png":
+                    return TypeOfFile.Image;
+                case ".jpg":
+                    return TypeOfFile.Image;
+                case ".jpeg":
+                    return TypeOfFile.Image;
+                case ".gif":
+                    return TypeOfFile.Gif;
+                case ".mp4":
+                    return TypeOfFile.Video;
+                default:
+                    throw new Exception("Unknown extension of file");
+            }
+        }
+    }
+}
---
+++ IcantHumor/Migrations/20230805090301_AddDateUploadForMedia.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace IcantHumor.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDateUploadForMedia : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateUpload",
+                table: "MediaFiles",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DateUpload",
+                table: "MediaFiles");
+        }
+    }
+}
